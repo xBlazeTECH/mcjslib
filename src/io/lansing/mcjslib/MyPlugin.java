72a2737fc7c8a59beb11c8a7643adad068b9c7dc
Lansing Nye-Madden <me@lansing.io>
2015-02-25 11:58:50 -0500

    Add JavaPlugin Override Methods
    I created auto generated JavaPlugin override methods.

--- src/io/lansing/mcjslib/MyPlugin.java
+++ src/io/lansing/mcjslib/MyPlugin.java
@@ -3,5 +3,23 @@ package io.lansing.mcjslib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyPlugin extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		super.onLoad();
+	}
 	
 }
