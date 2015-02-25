d99fc44fbe70f6ecb4c3076874c9dd2fec76590b
Lansing Nye-Madden <me@lansing.io>
2015-02-25 12:43:51 -0500

    Active Listener Functionality
    Checked Active Listener Functionality

---
+++ src/io/lansing/mcjslib/ActiveListener.java
@@ -0,0 +1,7 @@
+package io.lansing.mcjslib;
+
+import org.bukkit.event.Listener;
+
+public class ActiveListener implements Listener{
+	
+}
--- src/io/lansing/mcjslib/MyPlugin.java
+++ src/io/lansing/mcjslib/MyPlugin.java
@@ -7,9 +7,15 @@ public class MyPlugin extends JavaPlugin {
 	private RunningModule[] modules = {
 		// TODO Add Modules Here...	
 	};
+	private ActiveListener[] listeners = {
+		// TODO Add Listeners Here...
+	};
 	
 	@Override
 	public void onEnable() {
+		for (int i = 0; i < listeners.length; i++) {
+			this.getServer().getPluginManager().registerEvents(listeners[i], this);
+		}
 		for (int i = 0; i < modules.length; i++) {
 			modules[i].onEnable();
 		}
