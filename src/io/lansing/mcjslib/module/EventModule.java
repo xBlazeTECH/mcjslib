f9bb77522c59d395e7b31a6b618281d0c1755387
Lansing Nye-Madden <me@lansing.io>
2015-02-25 21:49:03 -0500

    New EventModule

---
+++ src/io/lansing/mcjslib/module/EventModule.java
@@ -0,0 +1,25 @@
+package io.lansing.mcjslib.module;
+
+import io.lansing.mcjslib.RunningModule;
+
+public class EventModule extends RunningModule {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
